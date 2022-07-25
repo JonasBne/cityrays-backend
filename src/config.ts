818ecb8f64884d8e60ae83bd6209aca6a4c70037
Jonas <boone.jonas@outlook.be>
2022-07-25 20:55:04 +0200

    refactor: cleanup

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,4 @@ import 'dotenv/config';
 export const config = {
   environment: process.env.NODE_ENV || 'development',
   port: process.env.PORT || '4000',
-  postgresUser: process.env.POSTGRES_USER,
-  postgresPassword: process.env.POSTGRES_PASSWORD,
 };
